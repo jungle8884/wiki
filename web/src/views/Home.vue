c2dfed3f1c06cabf1750873752189175d1b5a633
jungle8884 <jungle8884@163.com>
2022-11-22 00:24:53 +0800

    5-4 电子书布局调整, 一行显示3列

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -48,32 +48,21 @@
       <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
       >
-          <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-              <template #footer>
-                  <div><b>ant design vue</b> footer part</div>
-              </template>
+          <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
               <template #renderItem="{ item }">
-                  <a-list-item key="item.title">
+                  <a-list-item key="item.name">
                       <template #actions>
-          <span v-for="{ type, text } in actions" :key="type">
-            <component v-bind:is="type" style="margin-right: 8px" />
-            {{ text }}
-          </span>
-                      </template>
-                      <template #extra>
-                          <img
-                                  width="272"
-                                  alt="logo"
-                                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                          />
+                          <span v-for="{ type, text } in actions" :key="type">
+                            <component v-bind:is="type" style="margin-right: 8px" />
+                            {{ text }}
+                          </span>
                       </template>
                       <a-list-item-meta :description="item.description">
                           <template #title>
-                              <a :href="item.href">{{ item.title }}</a>
+                              <a :href="item.href">{{ item.name }}</a>
                           </template>
-                          <template #avatar><a-avatar :src="item.avatar"/></template>
+                          <template #avatar><a-avatar :src="item.cover"/></template>
                       </a-list-item-meta>
-                      {{ item.content }}
                   </a-list-item>
               </template>
           </a-list>
@@ -105,35 +94,32 @@
     name: 'home',
     setup() {
       console.log("setup");
-      const ebooks1 = ref();
-      const ebooks2 = reactive({books: []});
+      const ebooks = ref();
 
       // 生命周期函数: 先输出setup, 再渲染页面, 再输出onMounted
       onMounted(() => {
         console.log("onMounted");
         axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) => {
           const data = response.data;
-          ebooks1.value = data.content;
-          ebooks2.books = data.content;
+          ebooks.value = data.content;
           console.log(response);
         });
       });
 
       return {
-        ebooks1,
-        ebooks2: toRef(ebooks2, "books"),
-          listData,
-          pagination: {
-              onChange: (page: any) => {
-                  console.log(page);
-              },
-              pageSize: 3,
-          },
-          actions: [
-              { type: 'StarOutlined', text: '156' },
-              { type: 'LikeOutlined', text: '156' },
-              { type: 'MessageOutlined', text: '2' },
-          ],
+        ebooks,
+        listData,
+        pagination: {
+            onChange: (page: any) => {
+                console.log(page);
+            },
+            pageSize: 3,
+        },
+        actions: [
+            { type: 'StarOutlined', text: '156' },
+            { type: 'LikeOutlined', text: '156' },
+            { type: 'MessageOutlined', text: '2' },
+        ],
       }
     }
   });
